7f102cca5aea8fcc1a3507a82d7eefc71ff4027c
Sushant <khanalsushan14@gmail.com>
2024-07-21 10:41:30 +0545

    added hover effect in dashboard

--- src/App.js
+++ src/App.js
@@ -1,14 +1,19 @@
 import "./App.css";
-// import Login from "./Components/Assets/Login/Login";
-import Register from "./Components/Assets/Register/Register";
+// import Register from "./Components/Assets/Register/Register";
+//  import Login from "./Components/Assets/Login/Login";
 // import Navbar from "./Components/Navbar/Navbar";
 // import WhatWeOffer from "./Components/Offers/Offers";
 // import Footer from "./Components/Footer/Footer";
-
+import Dashboard from "./Components/Assets/Dashboard/Dashboard"
 function App() {
   return (
     <div className="App">
-      <Register />
+      <Dashboard />
+      {/* <Navbar />
+      <Login />
+      <WhatWeOffer />
+      <Footer /> */}
+
     </div>
   );
 }
